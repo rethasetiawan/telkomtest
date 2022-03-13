0115f128c198aac3f2b9206bd10e7cba1b723a61
margaretha <retha.setiawan@gmail.com>
2022-03-13 23:24:54 +0700

    get list repository from API

--- src/RepoList.js
+++ src/RepoList.js
@@ -1,13 +1,37 @@
 import React,{useState, useEffect} from "react";
+import axios from 'axios';
 
 const RepoList =(props)=>{
 
     const [name, setName] = useState('');
+    const [data, setData] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = async () => {
+        try {
+             setLoading(true);
+             const response = await axios.get(
+                 `https://api.github.com/users/${name}/repos`
+             );
+             console.log('response',response)
+             setData(response.data)
+         } catch (error) {
+             console.log('e',error)
+         } finally {
+             setLoading(false);
+         }
+     };
+
+    const handleSubmit = () =>{
+            fetchData();
+            console.log('data',data)
+    }
 
     const handleChangeName = (event) =>{
         console.log(event.target.value)
         setName(event.target.value)
     }
+
     return(
         <div style={styles.container} >
 					<div style={{alignItems:'center', flexDirection:'column', display:'flex'}}>
@@ -18,9 +42,18 @@ const RepoList =(props)=>{
 							onChange={(event) => handleChangeName(event)}
 							/>
 							</div>
-							<div>    Username : {name} </div>
+							<div>Username : {name} </div>
+							<button onClick={()=>handleSubmit()}>Submit</button>
 							<div style={{fontWeight:'bold',fontSize:20}}> Repository List </div>
 					</div>
+
+					{ data == [] ? 'null' :
+						data.map((data, index)=> (
+							<div key={index}>
+								{index+1}.{data.name}
+							</div>
+            ))
+           }
         </div>
     )
 }
